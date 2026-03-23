e54b0137c185cbbc8e089f84c236fffed37ed909
Panhathai Pongtong <ppongtong@gmail.com>
2026-03-22 20:49:15 -0400

    config: increase rate limit to 10 requests per 15 min

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5,
+    max: 10,
     standardHeaders: true,
     legacyHeaders: false,
     message: { error: "Too many requests, please try again later." },
